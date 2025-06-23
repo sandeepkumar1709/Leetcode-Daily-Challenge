90e8dcc82545f13615f70e233b143c48c4dbf58f
sandeepkumar1709 <sandeeppoloju0@gmail.com>
2025-06-23 04:36:49 -0400

    updating the logic

--- Feb_12_Max_Sum_of_a_Pair_With_Equal_Sum_of_Digits_.py
+++ Feb_12_Max_Sum_of_a_Pair_With_Equal_Sum_of_Digits_.py
@@ -1,3 +1,15 @@
+"""
+Approach:
+    The approach is to use a hashmap to store the sum of digits and the numbers that have that sum.
+    Then iterate over the hashmap and if the length of the list of numbers with the same sum is greater than 1, 
+    then we can find the maximum sum of two numbers with the same sum of digits. 
+    for sort we can use bisect.insort to keep the list sorted.
+Time complexity:
+    O(n log n) - to insert elements in the sorted order using bisect.
+Space complexity:
+    O(n) - to store the elements in the hashmap.
+"""
+
 def sum_of_digits(num):
         ans = 0
         while num > 0:
