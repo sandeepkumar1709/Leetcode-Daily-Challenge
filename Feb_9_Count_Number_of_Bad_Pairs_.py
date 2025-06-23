f6373771c43fe6132ad0c27803f190c8fa989a89
sandeepkumar1709 <sandeeppoloju0@gmail.com>
2025-06-23 04:32:42 -0400

    adding approach for a problem

--- Feb_9_Count_Number_of_Bad_Pairs_.py
+++ Feb_9_Count_Number_of_Bad_Pairs_.py
@@ -1,3 +1,17 @@
+"""
+Approach:
+    I couldn't able to get the part until I saw the hint in the problem statement. 
+    The hint suggests that we need to consider the difference between the value and its index. This means we can transform the array into a new array 
+    where each element is the original element minus its index. Then, we can count the number of good pairs in this transformed array.
+    (i.e. we need to check for good pairs first so get that we need to check for conditionn nums[j] -nums[i] == j - i, 
+    we can rearrange this to nums[j] - j == nums[i] - i)
+Time complexity:
+    O(n) - we iterate through the array once to create the transformed array and then again to count the good pairs.
+Space complexity:
+    O(n) - we use a dictionary to count the occurrences of each transformed value.
+"""
+
+
 def countBadPairs(nums) -> int:
 
     counter = {}
